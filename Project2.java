8818e8abd5714f860b14aa43e44d7cc1240dda9c
RuofanLiu <RuofanLiu@users.noreply.github.com>
2017-04-27 10:36:17 -0400

    Update Project2.java
    Parse for Non-contiguous added

--- Project2.java
+++ Project2.java
@@ -14,12 +14,14 @@ public class Project2 {
 		p.parseDataPM(args[0]);
 	}
 	
+	private HashMap<String, Process> processMap;
 	private PriorityQueue<Process> processQueue;
 	Comparator<Process> comparator;
 	
 	public Project2(){
 		comparator = new TimeComparator();
 		processQueue = new PriorityQueue<Process>(32, comparator);
+		processMap = new HashMap<String, Process>();
 	}
 	
 	/**
@@ -40,6 +42,31 @@ public class Project2 {
 			}
 			p.setStatus(true); 		//this line sets the initial process of a process to true, since it will enter the memory (please change the status whenever the process exits the memory)
 			processQueue.add(p);	//add the element to the queue, each element is an object of process.
+		}
+		reader.close();
+	}
+	
+	/**
+	 * the function parse data from the input text file (only for physical memory)
+	 * @param filename: the filename to be read
+	 * @throws IOException
+	 */
+	public void parseDataPM2(String filename) throws IOException{
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		String line = reader.readLine();
+		while((line = reader.readLine()) != null){
+			String[] token = line.split("/|\\ ");
+			Process p = new Process(token[0], Integer.parseInt(token[1]));
+			for(int i = 2; i < token.length; i++){	// this for loop adds the time interval to the ArrayList returned by the process class
+				if(i % 2 == 0){
+					p.getTimeList().add(Integer.parseInt(token[i]));
+				}
+				else{
+					p.getTimeList().add((Integer.parseInt(token[i]) + Integer.parseInt(token[i - 1])));
+				}
+			}
+			p.setStatus(true); //this line sets the initial process of a process to true, since it will enter the memory (please change the status whenever the process exits the memory)
+			processMap.put(token[0], p);	//add the element to the map, the key is the process ID, and the value is an object of Process class.
 		}
 		reader.close();
 	}
